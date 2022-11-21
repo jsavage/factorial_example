76fb6dbea35d8d5564e62900225e91ea0d9f6667
James Savage <consult.js@gmail.com>
2022-11-21 17:24:04 +0000

    Update test_fact.py

--- tests/test_fact.py
+++ tests/test_fact.py
@@ -1,8 +1,8 @@
 def test_answer2():
-    assert func(2) == 2
+    assert factorial(2) == 2
     
 def test_answer3():
-    assert func(3) == 6
+    assert factorial(3) == 6
     
 def test_answer4():
-    assert func(4) == 24
+    assert factorial(4) == 24
