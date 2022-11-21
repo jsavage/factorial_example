a8c5820e02faefcc7d17e438fb02402b1013782d
James Savage <consult.js@gmail.com>
2022-11-21 00:24:42 +0000

    working example

---
+++ factorial.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+#function computes factorial of a given number
+def factorial(n):
+	result = 1
+	i=1
+	while i<=n:
+		result*=i
+		i+=1
+	return result
+
+#read input from user
+n = int(input('Enter a number: '))
+
+#calculate factorial
+result = factorial(n)
+
+print(result)
